874928c4d393051ee0fa6256ef867d9e94d524b6
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-02 20:05:57 +0800

    Tool

--- Tool/Z.Infra.View/Gen.cs
+++ Tool/Z.Infra.View/Gen.cs
@@ -6,4 +6,9 @@ public class Gen : SourceGen
     {
         return this.AddClear().AddS("../../Module/").Add(this.Module).Add(this.TextInfra.PathCombine).Add(this.ClassName).AddS(".cl").AddResult();
     }
+
+    protected virtual String GetStatItemListFileName()
+    {
+        return this.AddClear().AddS("../../../Class/Out/net8.0/ToolData/Infra/ItemList").Add(this.StatItemClassName).AddS(".txt").AddResult();
+    }
 }
