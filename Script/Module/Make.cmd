c1ff41ae30d9a465ed3ee2ce609f437b52fae71e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-22 15:12:42 +0800

    Script

--- Script/Module/Make.cmd
+++ Script/Module/Make.cmd
@@ -3,7 +3,9 @@
 call Script\Module\CleanModule Type
 call Script\Module\CleanModule Draw
 call Script\Module\CleanModule Frame
+call Script\Module\CleanModule Infra
 
 call Script\Module\MakeModule Type
 call Script\Module\MakeModule Draw
 call Script\Module\MakeModule Frame
+call Script\Module\MakeModule Infra
