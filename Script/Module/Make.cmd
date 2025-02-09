dcb94e45f94ccd65306755601667968bcb6b5bfc
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-02-09 16:46:39 +0800

    Script

--- Script/Module/Make.cmd
+++ Script/Module/Make.cmd
@@ -1,10 +1,8 @@
 @echo off
 
-call Script\Module\MakeModule Draw.Draw
-call Script\Module\MakeModule View.Infra
-call Script\Module\MakeModule View.View
-call Script\Module\MakeModule Type.Type
-call Script\Module\MakeModule Frame.Frame
-call Script\Module\MakeModule Media.Play
-
-call Script\Module\MakeModule ViewDemo
+call Script\Module\MakeModule Draw
+call Script\Module\MakeModule Media
+call Script\Module\MakeModule Infra
+call Script\Module\MakeModule View
+call Script\Module\MakeModule Type
+call Script\Module\MakeModule Frame
--- Script/Module/MakeModule.cmd
+++ Script/Module/MakeModule.cmd
@@ -5,6 +5,6 @@ set DotNetOutFold=..\Class\Out\net8.0
 
 echo Make Module %Module%
 pushd %DotNetOutFold%
-saber make ../../../View/Module/%Module%
+saber make ../../../View/Module/View.%Module%
 echo Status: %errorlevel%
 popd
