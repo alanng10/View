9d1f4889f0ae0062a858d8346aa8ec48ac4dcde0
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-02-09 16:14:31 +0800

    Script

--- Script/Module/Make.cmd
+++ Script/Module/Make.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-call Script\Module\MakeModule Type
-call Script\Module\MakeModule Draw
-call Script\Module\MakeModule Frame
-call Script\Module\MakeModule Media
-call Script\Module\MakeModule Infra
-call Script\Module\MakeModule View
+call Script\Module\MakeModule Draw.Draw
+call Script\Module\MakeModule View.Infra
+call Script\Module\MakeModule View.View
+call Script\Module\MakeModule Type.Type
+call Script\Module\MakeModule Frame.Frame
+call Script\Module\MakeModule Media.Play
 
-call Script\Module\MakeDemo
+call Script\Module\MakeModule ViewDemo
